6fd032a6ec4c992214080f2fa08fb7013cee6bca
Nabin Thomas <nabin@youknowwhere>
2019-04-10 16:55:03 -0700

    Base Dockerfile added with ubuntu

--- server/main.py
+++ server/main.py
@@ -2,11 +2,9 @@ from flask import flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def mainPage():
 	return "Welcome to aMAZE.com";
-
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=80);
