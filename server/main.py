38d92939e4b8354e4c1c7df74ab5ceedfc2e3b83
Nabin Thomas <nabin@youknowwhere>
2019-04-10 09:36:58 -0700

    Adding main.py, requirements.txt

---
+++ server/main.py
@@ -0,0 +1,12 @@
+from flask import flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def mainPage():
+	return "Welcome to aMAZE.com";
+
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=80);
