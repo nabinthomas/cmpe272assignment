692e6b0e71f1bb2feedb65a9131aef4ca2786d16
Nabin Thomas <nabin@youknowwhere>
2019-04-12 14:21:47 -0700

    Docs update

--- server/main.py
+++ server/main.py
@@ -11,4 +11,5 @@ def mainPage():
 	return "<html> <title> aMAZE.com Online Book Store </title> <body> Welcome to aMAZE.com  </body> </html>";
 
 if __name__ == '__main__':
+	## Start the http server
 	app.run(host='0.0.0.0', port=80);
