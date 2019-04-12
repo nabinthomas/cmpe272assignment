5eb4170d0e6ad3b249cfd44cff66cb56ce65be77
Nabin Thomas <nabin@youknowwhere>
2019-04-11 19:14:30 -0700

    Doxygen style documentation in server code

--- server/main.py
+++ server/main.py
@@ -1,8 +1,12 @@
+## @package Main entry point for Web server.
+
 from flask import Flask
 
+## Create the App
 app = Flask(__name__)
 
 @app.route('/')
+## Handle request for default page. 
 def mainPage():
 	return "Welcome to aMAZE.com";
 
