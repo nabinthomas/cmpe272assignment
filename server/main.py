c7e526b123baa13fa43feead3ae7ac03a09ca32a
spanakka <spanakka@spanakka-m-26vc.attlocal.net>
2019-04-12 23:33:04 -0700

    Commiting intial cut of templatedlauncher script.

--- server/main.py
+++ server/main.py
@@ -1,6 +1,7 @@
 ## @package Main entry point for Web server.
-
-from flask import Flask
+  
+import datetime
+from flask import Flask, render_template
 
 ## Create the App
 app = Flask(__name__)
@@ -8,8 +9,21 @@ app = Flask(__name__)
 @app.route('/')
 ## Handle request for default page. 
 def mainPage():
-	return "<html> <title> aMAZE.com Online Book Store </title> <body> Welcome to aMAZE.com  </body> </html>";
+    now = datetime.datetime.now()
+    cur_hour = now.hour
+    if cur_hour < 12:
+        greet = "Good Morning "
+    elif cur_hour < 17:
+        greet = "Good Afternoon "
+    elif cur_hour < 20:
+        greet = "Good Evening "
+    else:
+        greet = "Good Night "
+
+    return render_template('template.html', my_time=now, my_greet=greet, my_string="Welcome to aMAZE.com!", my_title="aMAZE.com Online Book Store",\
+            my_list=["Binu Jose", "Ginto George", "Nabin Thomas", "Sandeep Panakkal"])
 
 if __name__ == '__main__':
-	## Start the http server
-	app.run(host='0.0.0.0', port=80);
+        ## Start the http server
+        app.run(host='0.0.0.0', port=3001);
+
