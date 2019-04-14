646992b430c877f30de2bbce1be7d0e0ecafc660
Nabin Thomas <nabin@youknowwhere>
2019-04-13 23:02:53 -0700

    Updated Template, main server code for time updates

--- server/main.py
+++ server/main.py
@@ -10,19 +10,13 @@ app = Flask(__name__)
 @app.route('/')
 ## Handle request for default page. 
 def mainPage():
-    now = datetime.datetime.now(pytz.timezone('US/Pacific'))
-    cur_hour = now.hour
-    if cur_hour < 12:
-        greet = "Good Morning "
-    elif cur_hour < 17:
-        greet = "Good Afternoon "
-    elif cur_hour < 20:
-        greet = "Good Evening "
-    else:
-        greet = "Good Night "
+    now = datetime.datetime.now(pytz.timezone('US/Pacific'));
 
-    return render_template('template.html', my_time=now, my_greet=greet, my_string="Welcome to Team aMAZE!", my_title="aMAZE.com Online Book Store",\
-            my_list=["Binu Jose", "Ginto George", "Nabin Thomas", "Sandeep Panakkal"])
+    return render_template('template.html', 
+			my_time=now, 
+			my_string="Welcome to Team aMAZE!", 
+			my_title="aMAZE.com Online Book Store",
+            my_list=["Binu Jose", "Ginto George", "Nabin Thomas", "Sandeep Panakkal"]);
 
 if __name__ == '__main__':
         ## Start the http server
