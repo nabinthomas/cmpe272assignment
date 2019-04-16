f1ae44e4d01f58582882c2afefa0c14d8da259d4
Nabin Thomas <nabin@youknowwhere>
2019-04-16 11:53:04 -0700

    Script to start servers

--- setup/startservers.sh
+++ setup/startservers.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Central place to start processes/services for the Web app.
+
 #Start Mongodb database server
 service mongodb start
 
 #Start the web server
 #This should be started at the end
-python3  app/server/main.py
+python3 app/server/main.py
