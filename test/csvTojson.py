ba94f0d526f4da1d737ac47cefc7b5f6da78751d
spanakkal <sandeep.panakkal@gmail.com>
2019-04-19 06:56:07 -0700

    Fixing file handling errors

--- test/csvTojson.py
+++ test/csvTojson.py
@@ -3,12 +3,12 @@ import csv
 import json
 
 if(len(sys.argv) == 3):
-    csvfile = open(sys.argv[1], 'r')
-    jsonfile = open(sys.argv[2], 'w')
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        print (json.dumps(row ))
-        jsonfile.write(json.dumps(row ))
+    with open(sys.argv[1], 'r') as csvfile:
+        with open(sys.argv[2], 'w') as jsonfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                print (json.dumps(row ))
+                jsonfile.write(json.dumps(row ))
 
 else :
     print ('Usage: ' + sys.argv[0] + ' input.csv output.jason ' + str(len(sys.argv)));
