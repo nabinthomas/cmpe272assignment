d4ebd6da91b5c375f41fb711682dfe2e0ba0094d
Nabin Thomas <nabin@youknowwhere>
2019-04-27 14:37:54 -0700

    remove imports from production server

--- setup/startservers.sh
+++ setup/startservers.sh
@@ -5,16 +5,6 @@
 #Start Mongodb database server
 service mongodb start
 
-#import test data to mongodb from csv after converting to json
-cd test
-#python3 csvTomongo.py books.csv 
-#python3 csvTomongo.py customers.csv 
-#python3 csvTomongo.py orders.csv  
-mongoimport --drop customers.json
-mongoimport --drop books.json
-mongoimport --drop orders.json
-
-cd ..
 #Start the web server
 #This should be started at the end
 python3 app/server/main.py  &
