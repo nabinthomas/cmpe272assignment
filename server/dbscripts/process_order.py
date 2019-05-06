590499832087b6148938592f4232bac776e0b30d
Nabin Thomas <nabin@youknowwhere>
2019-05-05 18:12:33 -0700

    marked TODO for deleting files

--- server/dbscripts/process_order.py
+++ server/dbscripts/process_order.py
@@ -3,6 +3,11 @@ import pymongo
 
 from server.dbscripts.update_inventory import * 
 
+###
+## TODO:  This file is to be removed. Fulfiling order is now handled in in fulfull_order.py
+## Other type of operations for orders are not planned now. 
+## 
+
 def import_books_inventory(db) :
     db.books.insert_one({  "Title" : "Fundamentals of Wavelets", "Author" : [ "Goswami, Jaideva", "Binu Jose" ], "Genre" : "signal_processing", "Page" : 228, "Publisher" : "Wiley", "Price" : 22.8, "ISBN-13" : "978-1503215672", "Inventory" : 10 })
     db.books.insert_one({  "Title" : "Data Smart", "Author" : [ "Foreman, John" ], "Genre" : "data_science", "Page" : 235, "Publisher" : "Wiley", "Price" : 23.5, "ISBN-13" : "978-1503215673", "Inventory" : 20 })
