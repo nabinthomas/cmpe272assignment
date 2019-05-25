8e936f6575dc3a0d8a94e8a4005d722a2cbc829b
Nabin Thomas <nabin@youknowwhere>
2019-05-24 22:33:57 -0700

    no more bash

--- setup/startservers.sh
+++ setup/startservers.sh
@@ -39,5 +39,5 @@ mongoimport -d test --drop ./test/unittests/data/customers.json
 #This should be started at the end
 export FLASK_ENV=development
 export SERVER_CONFIG=/root/app/server/config/settings.cfg
-PYTHONPATH=/root/app/ python3 app/server/main.py  mongodb://localhost/test & bash
+PYTHONPATH=/root/app/ python3 app/server/main.py  mongodb://localhost/test
 
